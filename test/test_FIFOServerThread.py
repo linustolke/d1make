7d12f40267f77a8849f380eda0448c7c31172136
Linus Tolke <linus@tigris.org>
2017-04-16 06:22:34 +0000

    Added test that does something.

--- test/test_FIFOServerThread.py
+++ test/test_FIFOServerThread.py
@@ -1,7 +1,13 @@
+import time
 import unittest
 
 from FIFOServerThread import FIFOServerThread
 
+
+class CommandMock(FIFOServerThread):
+    def call(self, command, args):
+        self.command = command
+        self.args = args
 
 class test_FIFOServerThread(unittest.TestCase):
     def test_open_and_close(self):
@@ -12,12 +18,21 @@ class test_FIFOServerThread(unittest.TestCase):
         t.start()
         t.close()
 
-    def test_fail_when_not_initialized(self):
+    def test_fail_when_not_started(self):
         try:
             FIFOServerThread().send("command", [])
             self.fail("Should have failed")
         except OSError:
             pass
+
+    def test_receive_command(self):
+        t = CommandMock()
+        t.start()
+        t.send("gupta", ("trade", 17,))
+        time.sleep(0.2)
+        self.assertEqual(t.command, "gupta")
+        self.assertEqual(t.args, ("trade", 17,))
+        t.close()
 
 
 if __name__ == "__main__":
